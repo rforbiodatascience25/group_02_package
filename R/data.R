be30e748ef5fd4f47cd9ac0543268e0f0f84ef1f
AnastasiaKampantai <s253724@dtu.dk>
2025-10-28 09:22:58 +0100

    data file update, s253427

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' World Health Organization TB data
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `amino_acids`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{codons}{3 nucleotides related to an amino acid}
+
+#'   ...
+#' }
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"amino_acids"
