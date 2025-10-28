234c794eecf3a656e100b19de41fed28b0ed4204
hannahnorrelykke <s224972@dtu.dk>
2025-10-28 10:20:11 +0100

    Documentation for split_into_codons function

--- R/split_into_codons.R
+++ R/split_into_codons.R
@@ -1,3 +1,16 @@
+#' Split a sequence into codons
+#'
+#' @param sequence The sequence to be split into codons. Should be a character vector
+#' @param start The starting position. Positions before are not split into codons and not returned. Should be integer
+#'
+#' @returns A character vector with codons
+#'
+#' @examples
+#' split_into_codons("AUGUUUUAA")
+#'
+#' @export
+
+
 split_into_codons <- function(sequence, start = 1){
   length <- nchar(sequence)
   codons <- substring(sequence,
